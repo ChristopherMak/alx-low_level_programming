76b8b22c25603deed745d44f2e8e428e84af6e5a
ChristopherMak <makeketachristopher@gmail.com>
2023-10-07 05:50:52 -0700

    Prints Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,13 @@
 /**
  * main - Prints numbers from 0 to 9.
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; 1 < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
