7e1af5c3cef21a80c67be8aa211ce9627a528063
ChristopherMak <makeketachristopher@gmail.com>
2023-10-17 05:30:52 -0700

    Swapped integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps thw values of the integers
+ * @a: The first integer to be swapped
+ * @b: The seconfd integer to wapped
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp - *a;
+	*a = *b;
+	*b= temp;
+}
+
