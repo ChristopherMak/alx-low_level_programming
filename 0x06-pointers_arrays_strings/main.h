5d30886f8c89189d217ac0e1f42da43edb81ee47
ChristopherMak <makeketachristopher@gmail.com>
2023-10-19 15:54:51 -0700

    Adds two number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,8 @@ char *cap_string(char *);
 char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
-char infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void rev_string(char *n);
 
 #endif
