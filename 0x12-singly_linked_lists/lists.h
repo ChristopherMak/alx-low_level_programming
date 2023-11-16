0f399b23d8896c72162bd3cea4363d7b00c99c39
ChristopherMak <makeketachristopher@gmail.com>
2023-11-16 13:57:25 -0800

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct list_s - A singly linked list
  * @str: string or malloced string
@@ -15,7 +19,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -23,6 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _putchar(char c);
 
 #endif
